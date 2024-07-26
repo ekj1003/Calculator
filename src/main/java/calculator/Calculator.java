7f05e520674d8348ce1d8ec81c9067dd4085fa3f
Robbie <robbie331@>
2024-07-26 15:54:50 +0900

    Level2 - 7 calculateCircleArea 원넓이 계산 메서드  inquiryAreaResults 조회 메서드, areaResults, circleArea, PI 변수 선언

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,27 +1,42 @@
 package calculator;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Calculator {
+    // < 변수 >
     private double result = 0.0;
     private List<Double> results;
+    private double circleArea;
+    private List<Double> areaResults;
+    static final double PI = 3.141592; // 원 넓이를 구할 때 PI 값은 상수이므로 static final 사용
 
+
+
+    // < 생성자 >
     // 생성자를 통해 results 초기화
     public Calculator() {
         this.results = new ArrayList<>();
-    }
-    public double getResult() {
-        return this.result;
+        this.areaResults = new ArrayList<>();
     }
 
+
+    // < Getter, Setter >
+    public double getResult() { return this.result; }
     public void setResult(double res) {
         this.result = res;
     }
-
     public List<Double> getResults() {
         return results;
     }
+    public void setResults(List<Double> res) { this.results = res; }
+    public double getCircleArea() { return this.circleArea; }
+    public void setCircleArea(double area) { this.circleArea = area; }
+    public List<Double> getAreaResults() { return this.areaResults; }
+
+
+
+
+    // < 기능 메서드 >
     public void calculate(int num1, int num2, char operator) {
 
         switch (operator) {
@@ -54,6 +69,17 @@ public class Calculator {
 
     public void inquiryResults() {
         for (double element : this.results) {
+            System.out.println(element);
+        }
+    }
+
+    public void calculateCircleArea(double radius) {
+        circleArea = Math.pow(radius, 2) * PI;
+        areaResults.add(circleArea);
+    }
+
+    public void inquiryAreaResults() {
+        for (double element : areaResults) {
             System.out.println(element);
         }
     }
