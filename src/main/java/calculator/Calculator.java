890d03149036cef0e55453e9d05dfbd1a9243d19
Robbie <robbie331@>
2024-07-26 10:54:11 +0900

    Level2 - 1 두 숫자, 연산자를 받아 연산 수행 및 나누기 에러 처리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,4 +2,26 @@ package calculator;
 
 public class Calculator {
 
+    double result;
+
+    Exception exception;
+    public double calcuate(int num1, int num2, char operator) {
+        if (operator == '+') {
+            this.result = num1 + num2;
+        } else if (operator == '-') {
+            this.result = num1 - num2;
+        } else if (operator == '*') {
+            this.result = num1 * num2;
+        } else if (operator == '/') {
+            if (num2 == 0) {
+                exception = new Exception(1);
+                exception.exceptionNotice();
+            }
+            this.result = num1 / num2;
+        }
+
+        return this.result;
+
+    }
+
 }
