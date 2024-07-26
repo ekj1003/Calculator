929deae923eaccb73d995cee324097e5250f8cf3
Robbie <robbie331@>
2024-07-26 14:30:02 +0900

    Level2 - 6 Calculator class에 results를 옮기면서 조회, 삭제 기능 적용

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,7 +7,6 @@ public class App {
         Scanner sc = new Scanner(System.in);
         // calculator class 선언
         Calculator calculator = new Calculator();
-        List<Double> resArr = new ArrayList<>();
 
         while (true) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -20,23 +19,21 @@ public class App {
 
             // Calculator 클래스의 calculate 메소드 사용하여 연산
             calculator.calculate(num1, num2, operator);
-            double result = calculator.getResult();
-            resArr.add(result);
 
-            System.out.println("결과: " + result);
+            System.out.println("결과: " + calculator.getResult());
             // 버퍼 비우기
             sc.nextLine();
 
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             String removeFlag = sc.nextLine();
-            if (removeFlag.equals("remove") && !resArr.isEmpty()) {
-                resArr = calculator.removeResult(resArr);
+            if (removeFlag.equals("remove") && !calculator.getResults().isEmpty()) {
+                calculator.removeResult();
             }
 
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquire 입력 시 조회)");
             String inquiryFlag = sc.nextLine();
             if (inquiryFlag.equals("inquire")) {
-                calculator.inquiryResults(resArr);
+                calculator.inquiryResults();
             }
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
