04f82b28e96511ecd386415ee72d92c71c5fb9fe
Robbie <robbie331@>
2024-07-26 16:22:56 +0900

    Level2 - 8 Arithmetic, Circle Calculator class 생성으로 코드 수정

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -4,62 +4,68 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) {
-        // calculator class 선언
-        Calculator calculator = new Calculator();
+        // 각각의 계산기 선언
+        ArithmeticCalculator arithmeticCalculator = new ArithmeticCalculator();
+        CircleCalculator circleCalculator = new CircleCalculator();
         Scanner sc = new Scanner(System.in);
         int menu;
 
         while (true) {
-            System.out.println("메뉴를 선택하세요.(1: 원넓이 계산, 2:사칙연산)");
+            System.out.println("메뉴를 선택하세요(1: 원넓이 계산, 2:사칙연산).");
             menu = sc.nextInt();
             sc.nextLine(); // 개행문자 제거
-            if(menu == 1) {
+
+            if (menu == 1) {
                 System.out.print("반지름을 입력하세요: ");
                 double r = sc.nextDouble();
                 sc.nextLine(); // 개행문자 제거
-                calculator.calculateCircleArea(r);
+                circleCalculator.calculateCircleArea(r);
 
-                System.out.println("결과: "+ calculator.getCircleArea());
+                System.out.println("결과: " + circleCalculator.getCircleArea());
 
                 System.out.print("원 넓이 결과를 조회하시겠습니까? (inquire 입력 시 조회): ");
                 String inquiryFlag = sc.nextLine();
                 if (inquiryFlag.equals("inquire")) {
-                    calculator.inquiryAreaResults();
+                    circleCalculator.inquiryAreaResults();
                 }
-            }
 
-            else {
+                System.out.print("더 계산하시겠습니까? (exit 입력 시 종료): ");
+                String flagExit = sc.nextLine();
+                if (flagExit.equals("exit")) break;
+            } else if (menu == 2) {
                 System.out.print("첫 번째 숫자를 입력하세요: ");
                 int num1 = sc.nextInt();
+                sc.nextLine(); // 개행문자 제거
+
                 System.out.print("두 번째 숫자를 입력하세요: ");
                 int num2 = sc.nextInt();
+                sc.nextLine(); // 개행문자 제거
 
                 System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, /
                 char operator = sc.next().charAt(0);
+                sc.nextLine(); // 개행문자 제거
 
-                // Calculator 클래스의 calculate 메소드 사용하여 연산
-                calculator.calculate(num1, num2, operator);
+                // ArithmeticCalculator 클래스의 calculate 메소드 사용하여 연산
+                arithmeticCalculator.calculate(num1, num2, operator);
 
-                System.out.println("결과: " + calculator.getResult());
-                // 버퍼 비우기
-                sc.nextLine();
+                System.out.println("결과: " + arithmeticCalculator.getResult());
 
-                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                System.out.print("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제): ");
                 String removeFlag = sc.nextLine();
-                if (removeFlag.equals("remove") && !calculator.getResults().isEmpty()) {
-                    calculator.removeResult();
+                if (removeFlag.equals("remove") && !arithmeticCalculator.getResults().isEmpty()) {
+                    arithmeticCalculator.removeResult();
                 }
 
-                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquire 입력 시 조회)");
+                System.out.print("저장된 연산결과를 조회하시겠습니까? (inquire 입력 시 조회): ");
                 String inquiryFlag = sc.nextLine();
                 if (inquiryFlag.equals("inquire")) {
-                    calculator.inquiryResults();
+                    arithmeticCalculator.inquiryResults();
                 }
-            }
 
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            String flagExit = sc.nextLine();
-            if (flagExit.equals("exit")) break;
+                System.out.print("더 계산하시겠습니까? (exit 입력 시 종료): ");
+                String flagExit = sc.nextLine();
+                if (flagExit.equals("exit")) break;
+            }
         }
         sc.close();
     }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,79 +3,40 @@ package calculator;
 import java.util.*;
 
 public class Calculator {
-    // < 변수 >
-    private double result = 0.0;
-    private List<Double> results;
-    private double circleArea;
-    private List<Double> areaResults;
-    static final double PI = 3.141592; // 원 넓이를 구할 때 PI 값은 상수이므로 static final 사용
+    protected List<Double> results;
+    protected List<Double> areaResults;
 
-
-
-    // < 생성자 >
-    // 생성자를 통해 results 초기화
     public Calculator() {
         this.results = new ArrayList<>();
         this.areaResults = new ArrayList<>();
     }
 
-
-    // < Getter, Setter >
-    public double getResult() { return this.result; }
-    public void setResult(double res) {
-        this.result = res;
-    }
     public List<Double> getResults() {
         return results;
     }
-    public void setResults(List<Double> res) { this.results = res; }
-    public double getCircleArea() { return this.circleArea; }
-    public void setCircleArea(double area) { this.circleArea = area; }
-    public List<Double> getAreaResults() { return this.areaResults; }
 
+    public void setResults(List<Double> results) {
+        this.results = results;
+    }
 
+    public List<Double> getAreaResults() {
+        return areaResults;
+    }
 
-
-    // < 기능 메서드 >
-    public void calculate(int num1, int num2, char operator) {
-
-        switch (operator) {
-            case '+':
-                this.result = num1 + num2;
-                break;
-            case '-':
-                this.result = num1 - num2;
-                break;
-            case '*':
-                this.result = num1 * num2;
-                break;
-            case '/':
-                if (num2 == 0) {
-                    throw new ArithmeticException("0으로 나눌 수 없습니다.");
-                } else {
-                    this.result = (double) num1 / num2;
-                }
-                break;
-            default:
-                throw new IllegalArgumentException("잘못된 연산자입니다.");
-        }
-
-        this.results.add(this.result);
+    public void setAreaResults(List<Double> areaResults) {
+        this.areaResults = areaResults;
     }
 
     public void removeResult() {
-        this.results.remove(0);
-    }
-
-    public void inquiryResults() {
-        for (double element : this.results) {
-            System.out.println(element);
+        if (!results.isEmpty()) {
+            results.remove(0);
         }
     }
 
-    public void calculateCircleArea(double radius) {
-        circleArea = Math.pow(radius, 2) * PI;
-        areaResults.add(circleArea);
+    public void inquiryResults() {
+        for (double element : results) {
+            System.out.println(element);
+        }
     }
 
     public void inquiryAreaResults() {
