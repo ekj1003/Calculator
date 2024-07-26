6dadc1029469e74b64d82fba38f3b119b4281830
Robbie <robbie331@>
2024-07-26 11:27:25 +0900

    Level2 - 1 Exception throw 하기

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,26 +2,29 @@ package calculator;
 
 public class Calculator {
 
-    double result;
-
-    Exception exception;
-    public double calcuate(int num1, int num2, char operator) {
-        if (operator == '+') {
-            this.result = num1 + num2;
-        } else if (operator == '-') {
-            this.result = num1 - num2;
-        } else if (operator == '*') {
-            this.result = num1 * num2;
-        } else if (operator == '/') {
-            if (num2 == 0) {
-                exception = new Exception(1);
-                exception.exceptionNotice();
-            }
-            this.result = num1 / num2;
+    public double calculate(int num1, int num2, char operator) {
+        double result;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    throw new ArithmeticException("0으로 나눌 수 없습니다.");
+                } else {
+                    result = (double) num1 / num2;
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("잘못된 연산자입니다.");
         }
-
-        return this.result;
-
+        return result;
     }
-
 }
+
